e2368f37919890202a3a3925f7dc82effad71bbe
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-10-16 14:27:33 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class app(models.Model):
+class pessoas(models.Model):
 
     """
     ->
@@ -13,7 +13,6 @@ class app(models.Model):
         ('ativo', 'ATIVO'),
         ('inativo', 'INATIVO'),
     )
-
     nome = models.CharField('Nome', max_length = 255)
     email = models.EmailField('E-mail')
     telefone = models.CharField('Telefone', max_length = 14)
@@ -45,3 +44,49 @@ class app(models.Model):
         verbose_name = 'CADASTRO'
         verbose_name_plural = 'CADASTROS'
         ordering = ['nome'] 
+ 
+
+class estoque(models.Model):
+
+    """
+    ->
+    :return:
+    """
+
+    STATUS = (
+        ('ativo', 'DISPONIVEL'),
+        ('inativo', 'ESGOTADO'),
+    )
+    tipo = models.CharField('Tipo', max_length = 255)
+    produto = models.CharField('Produto', max_length = 255)
+    preco = models.FloatField('Preço')
+    custo = models.FloatField('Custo')
+    quantidade = models.IntField('Quantidade')
+    status = models.CharField(
+        max_length = 10,
+        choices = STATUS
+    )
+    criado = models.DateTimeField(auto_now_add=True)
+    atualizado = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+
+        """
+        ->
+        :return:
+        """
+
+        return self.produto
+
+
+    class Meta:
+
+        """
+        ->
+        :return:
+        """
+
+        verbose_name = 'PRODUTO'
+        verbose_name_plural = 'PRODUTOS'
+        ordering = ['produto'] 
+        
