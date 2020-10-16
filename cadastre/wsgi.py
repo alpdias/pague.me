d788d3fd861ca3706450c30829d8ead7a343e1c2
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-10-16 13:03:57 -0300

    Update wsgi.py

--- cadastre/wsgi.py
+++ cadastre/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cadastre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buy.settings')
 
 application = get_wsgi_application()
