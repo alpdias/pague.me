d9d2154af507bc156801698b3a2932f4fb455d13
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-10-16 13:02:51 -0300

    Update asgi.py

--- cadastre/asgi.py
+++ cadastre/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cadastre.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'buy.settings')
 
 application = get_asgi_application()
