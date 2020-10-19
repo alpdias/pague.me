eac91ce58493b141f7218eb162f9fd3408fa880a
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-10-19 14:34:11 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,20 +9,14 @@ class pessoas(models.Model):
     :return:
     """
 
-    STATUS = (
-        ('ativo', 'ATIVO'),
-        ('inativo', 'INATIVO'),
-    )
+    STATUS = (('ativo', 'ATIVO'), ('inativo', 'INATIVO'))
     nome = models.CharField('Nome', max_length = 255)
     telefone = models.CharField('Telefone', max_length = 14)
     email = models.EmailField('E-mail')
     cpf = models.CharField('CPF', max_length = 14)
     endereco = models.CharField('Endereço', max_length = 255)
     observacao = models.TextField('Observações')
-    status = models.CharField(
-        max_length = 7,
-        choices = STATUS
-    )
+    status = models.CharField(max_length = 7, choices = STATUS)
     criado = models.DateTimeField('Criado em', auto_now_add = True)
     atualizado = models.DateTimeField('Atualizado em', auto_now = True)
 
@@ -55,19 +49,13 @@ class estoque(models.Model):
     :return:
     """
 
-    STATUS = (
-        ('disponivel', 'DISPONIVEL'),
-        ('esgotado', 'ESGOTADO'),
-    )
+    STATUS = (('disponivel', 'DISPONIVEL'), 'esgotado', 'ESGOTADO'))
     produto = models.CharField('Produto', max_length = 255)
     preco = models.DecimalField('Preço', max_digits = 999, decimal_places = 2)
     custo = models.DecimalField('Custo', max_digits = 999, decimal_places = 2)
     quantidade = models.IntegerField('Quantidade')
     descricao = models.TextField('Descrição')
-    status = models.CharField(
-        max_length = 10,
-        choices = STATUS
-    )
+    status = models.CharField(max_length = 10, hoices = STATUS)
     criado = models.DateTimeField('Criado em', auto_now_add = True)
     atualizado = models.DateTimeField('Atualizado em', auto_now = True)
 
@@ -100,29 +88,12 @@ class vendas(models.Model):
     :return:
     """
     
-    PAGAMENTO = (
-        ('dinhero', 'DINHEIRO),
-        ('debito', 'DEBITO'),
-        ('credito', 'CREDITO'),
-    )
-    STATUS = (
-        ('aberto', 'ABERTO'),
-        ('fechado', 'FECHADO'),
-    )
+    STATUS = (('aberto', 'ABERTO'), ('fechado', 'FECHADO'))
     cliente = models.CharField('Cliente', max_length = 255)
     valor = models.DecimalField('Valor', max_digits = 999, decimal_places = 2)
-    pagamento = models.CharField(
-        max_digits = 8, 
-        choice = PAGAMENTO
-    )
-    comprovante = models.FileField(
-        upload_to='pdf',
-        verbose_name = 'Comprovante'
-    )
-    status = models.CharField(
-        max_length = 7,
-        choices = STATUS
-    )
+    pagamento = models.CharField('Pagamento', max_digits = 8)
+    comprovante = models.FileField(upload_to='pdf', verbose_name = 'Comprovante')
+    status = models.CharField(max_length = 7, choices = STATUS)
     criado = models.DateTimeField('Criado em', auto_now_add = True)
     atualizado = models.DateTimeField('Atualizado em', auto_now = True)
 
