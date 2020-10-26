21b99ba312d8ac392790d8d64928e28494eff867
Paulo Alcântara <52184239+alpdias@users.noreply.github.com>
2020-10-26 13:02:28 -0300

    Create forms.py

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import pessoas, estoque
+
+class pessoasForm(forms.ModelForm):
+  
+  class Meta:
+    model = pessoas
+    fields = ('nome', 'telefone', 'email', 'cpf', 'endereco', 'observacao', 'status')  
+    
+    
+class estoqueForm(forms.ModelForm):
+  
+  class Meta:
+    model = estoque
+    fields = ('produto', 'preco', 'quantidade', 'descricao', 'fornecedor', 'status')
+    
+  
